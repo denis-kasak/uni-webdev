7bc995e586edb4f70c6d1c71673451a0abfcc17c
instantcreator <dominik_czerwinski@yahoo.com>
2023-03-07 16:54:25 +0100

    Fixed cookies and file delivery.

--- server/index.js
+++ server/index.js
@@ -2,32 +2,75 @@
 
 
 const express = require("express");
-
 const cookieParser = require("cookie-parser");
 var bodyParser = require('body-parser');
+const { v4: uuid } = require("uuid");
 const app = express();
-var sqlmodule = require("./sql-module");
-
+const sqlmodule = require("./sql-module");
+var basepath = __dirname.replace("\server","\html");
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('uni-webdev'));
 
 sqlmodule.generateDatabase();
 
 app.use(express.static("Webentwicklung"));
 app.get('/', (req, res) => {
-	if(req.cookies.user == undefined){
-		res.cookie("user", Date.now().toString, {expires : Number.MAX_SAFE_INTEGER});
+	if(req.cookies.user === undefined){
+		const d = new Date();
+    	d.setTime(d.getTime() + (20*365*24*60*60*1000));
+		var userid = uuid()
+		res.cookie("user", userid , { expires: d, httpOnly: true });
+		sqlmodule.addUser(userid);
 	}
-	if(req.cookies.session == undefined){
-		res.cookie("session", "active session by" + req.cookies.user.toString());
-	}
-	    res.sendFile('/index.html', {root: __dirname });
+	/*if(req.cookies.session === undefined){
+		res.cookie("session", "active session by" + req.cookies.user);
+	}*/	
+		console.log(basepath);
+		console.log(__dirname);
+	    res.sendFile('/index.html', {root: basepath });
 });
 
-app.get('/Impressum', (req, res) => {
-	if(req.cookies.user == undefined){
+app.get('/impressum', (req, res) => {
+	if(req.cookies.user === undefined){
 		res.redirect('/');
 	}else{
-		res.sendFile('/Impressum.html', {root: __dirname });
+		res.sendFile('/impressum.html', {root: basepath });
 	}
 });
 
 app.listen(8081);
+/*var users = [];
+var Kommentare = [];
+var Favouriten = [];
+let Names = ["AnonymesZebra", "AnonymerGorilla", "AnonymesKänguru", "AnonymesChinchilla"];
+function createUser(){
+	var Name = Names[Math.floor(Math.random()*Names.length)];
+	var id = uuid();
+	var Username = Name + id;
+	const newUser = { id: id, Username: Username};	
+	users.push(newUser);
+}
+function createKommentar(usercookie,paramKommentar){
+	var usera = users.find((user) => user.id === paramuser);
+	var user = usera.Username;
+	const newKommentar = {id: id, Username: user,userid: usercookie, Kommentar: paramKommentar,Zeit:  Zeitstempel() }
+	Kommentare.push(newKommentar);
+}
+function Zeitstempel(){
+  var heute = new Date();
+  var Datum = heute.getDate();
+  var StundenZahl = heute.getHours();
+  var MinutenZahl = heute.getMinutes();
+  var SekundenZahl = heute.getSeconds();
+  return Datum.toString() + " " + StundenZahl.toString()+ ":" + MinutenZahl.toString() + ":" + SekundenZahl.toString;
+}
+function getKommentar(id){
+	return Kommentare.find((Kommentar) => Kommentar.id === id)
+}
+
+function getallKommentarebyUser(usercookie){
+	return Kommentare.find((Kommentar) => Kommentar.id === usercookie)
+}
+*/
+
--- server/sql-module.js
+++ server/sql-module.js
@@ -1,193 +1,207 @@
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'passwort',
-    database: 'vergleich24'
-});
-
-var mysql = require('mysql');
-
-let createKommentare = "Create Table Kommentare  ( user varchar(3000), Kommentar varchar(2000));";
-let createFavoriten= "Create Table Favoriten(id int auto_increment primary key, user varchar(3000),Favouritenbeschreibung varchar(1000) ,Favoritenquery varchar(2000));";
-let createUser="Create table User (id int auto_increment primary key,user varchar(3000) );";
-let createPortal = "Create table Portale (name varchar(3000), Beschreibung varchar(3000));"
-
-
-
-function generateDatabase(){
-
-    connection.connect(function(err){
-        if(err){
-            return console.error('error '+err.message);
-        }
-    
-        console.log("connected to the mysql server.");
-    
-        connection.query(createKommentare, function(err, results, fields) {
-            if (err) {
-              console.log(err.message);
-            }
-          });
-          connection.query(createFavoriten, function(err, results, fields) {
-            if (err) {
-              console.log(err.message);
-            }
-          });
-          connection.query(createUser, function(err, results, fields) {
-            if (err) {
-              console.log(err.message);
-            }
-          });
-
-          connection.query(createPortal, function(err, results, fields){
-            if(err){
-                console.log(err.message);
-            }
-          });
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'passwort',
+        database: 'vergleich24'
     });
-}
 
-function getFavorite(favId){
-    let sql = "SELECT * FROM Favoriten WHERE id="+favId+";";
 
-    connection.connect(sql,function(err){
-        if(err){
-            console.log(err.message);
-        }
 
-        connection.query(sql, function(err, results, _){
+    let createKommentare = "Create Table Kommentare  ( user varchar(3000), Kommentar varchar(2000));";
+    let createFavoriten= "Create Table Favoriten(id int auto_increment primary key, user varchar(3000),Favouritenbeschreibung varchar(1000) ,Favoritenquery varchar(2000));";
+    let createUser="Create table User (id int auto_increment primary key,user varchar(3000), anzeigename varchar(3000) );";
+    let createPortal = "Create table Portale (name varchar(3000), Beschreibung varchar(3000));";
+   module.exports.generateDatabase = function generateDatabase(){
+
+        connection.connect(function(err){
             if(err){
-                console.log(err.message);
+                return console.error('error '+err.message);
             }
-
-            return results;
-        });
-    });
-}
-
-//User = UserCoockie
-function setFavorite(user, beschreibung, favoritenquery){
-    let sql = "INSERT INTO Favoriten(user, Favouritenbeschreibung, Favoritenquery)VALUES('"+user+"', '"+beschreibung+"', '"+favoritenquery+"');";
-
-    connection.connect(function(err){
-        if(err){
-            console.log(err.message);
-        }
-
-        connection.query(sql, function(err, results, _){
-            if(err){
-                console.log(err.message);
-            }
-
-            return results;
-        });
-    });
-}
-
-function getUsername(userid){
-    let sql = "SELECT user FROM User WHERE id="+userid+";";
-
-    connection.connect(function(err){
-        if(err){
-            console.log(err.message);
-        }
-
-        connection.query(sql, function(err, results, _){
-            if(err){
-                console.log(err);
-            }
-
-            return results;
-
-        });
-
-    });
-}
-
-//Random Username wird gespeichert
-function addUser(username){
-let sql = "INSERT INTO User(user) VALUES('"+username+"');";
-connection.connect(function(err){
-    if(err){
-        console.log(err.message);
-    }
-    connection.query(sql, function(err, results, _){
-        if(err){
-            console.log(err.message);
-        }
-    });
-});
-}
-
-function getKommentare(){
-    let sql = "SELECT * FROM Kommentare;";
-
-    connection.connect(function(err){
-        if(err){
-            console.log(err.message);
-        }
-
-        connection.query(sql, function(err, results, _){
-            if(err){
-                console.log(err.message);
-            }
-
-            return results;
-        });
-    });
-}
-
-function setKommentar(user, kommentar){
-    let sql = "INSERT INTO Kommentare(user, Kommentar) VALUES('"+user+"','"+kommentar+"');";
-    connection.connect(function(err){
-        if(err){
-            console.log(err);
-        }
-
-        connection.query(sql, function(err, results, _){
-            if(err){
-                console.log(err);
-            }
-            return results;
-        });
         
-    });
-}
+            console.log("connected to the mysql server.");
+        
+            connection.query(createKommentare, function(err, results, fields) {
+                if (err) {
+                  console.log(err.message);
+                }
+              });
+              connection.query(createFavoriten, function(err, results, fields) {
+                if (err) {
+                  console.log(err.message);
+                }
+              });
+              connection.query(createUser, function(err, results, fields) {
+                if (err) {
+                  console.log(err.message);
+                }
+              });
 
-//Eigene Table
-function getBeschreibung(name){
-let sql = "SELECT Beschreibung FROM Portale WHERE name = '"+name+"'";
-
-connection.connect(function(err){
-    if(err){
-        console.log(err.message);
+              connection.query(createPortal, function(err, results, fields){
+                if(err){
+                    console.log(err.message);
+                }
+              });
+        });
     }
 
-    connection.query(sql, function(err, results, _){
+    module.exports.getFavorite = function getFavorite(favId){
+        let sql = "SELECT * FROM Favoriten WHERE id="+favId+";";
+
+        connection.connect(sql,function(err){
+            if(err){
+                console.log(err.message);
+            }
+
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err.message);
+                }
+
+                return results;
+            });
+        });
+    }
+
+    //User = UserCoockie
+    module.exports.setFavorite =  function setFavorite(user, beschreibung, favoritenquery){
+        let sql = "INSERT INTO Favoriten(user, Favouritenbeschreibung, Favoritenquery)VALUES('"+user+"', '"+beschreibung+"', '"+favoritenquery+"');";
+
+        connection.connect(function(err){
+            if(err){
+                console.log(err.message);
+            }
+
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err.message);
+                }
+
+                return results;
+            });
+        });
+    }
+
+    module.exports.getUsername = function getUsername(userid){
+        let sql = "SELECT user FROM User WHERE id="+userid+";";
+
+        connection.connect(function(err){
+            if(err){
+                console.log(err.message);
+            }
+
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err);
+                }
+
+                return results;
+
+            });
+
+        });
+    }
+
+    //Random Username wird gespeichert
+    module.exports.addUser = function addUser(username){
+    let sql = "INSERT INTO User(user) VALUES('"+username+"');";
+    connection.connect(function(err){
+        if(err){
+            console.log(err.message);
+        }
+        connection.query(sql, function(err, results, _){
+            if(err){
+                console.log(err.message);
+            }
+        });
+    });
+    }
+
+    module.exports.updateAnzeigename = function updateAnzeigename(usercookie, anzeigename){
+        let sql = "Update User set anzeigename = '"+anzeigename+"' where user = '"+usercookie+"';";
+        connection.connect(function(err){
+            if(err){
+                console.log(err.message);
+            }
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err.message);
+                }
+            });
+        });
+    }
+
+    module.exports.getKommentare = function getKommentare(){
+        let sql = "SELECT * FROM Kommentare;";
+
+        connection.connect(function(err){
+            if(err){
+                console.log(err.message);
+            }
+
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err.message);
+                }
+
+                return results;
+            });
+        });
+    }
+
+    module.exports.setKommentar = function setKommentar(user, kommentar){
+        let sql = "INSERT INTO Kommentare(user, Kommentar) VALUES('"+user+"','"+kommentar+"');";
+        connection.connect(function(err){
+            if(err){
+                console.log(err);
+            }
+
+            connection.query(sql, function(err, results, _){
+                if(err){
+                    console.log(err);
+                }
+                return results;
+            });
+
+        });
+    }
+
+    //Eigene Table
+    module.exports.getBeschreibung = function getBeschreibung(name){
+    let sql = "SELECT Beschreibung FROM Portale WHERE name = '"+name+"'";
+
+    connection.connect(function(err){
         if(err){
             console.log(err.message);
         }
 
-        return results;
+        connection.query(sql, function(err, results, _){
+            if(err){
+                console.log(err.message);
+            }
+
+            return results;
+        });
     });
-});
-}
-
-function getAllFavoriten(user){
-let sql = "SELECT * FROM Favoriten WHERE user='"+user+"';";
-
-connection.connect(function(err){
-    if(err){
-        console.log(err);
     }
 
-    connection.query(sql, function(err, results, _){
+    module.exports.getAllFavoriten = function getAllFavoriten(user){
+    let sql = "SELECT * FROM Favoriten WHERE user='"+user+"';";
+
+    connection.connect(function(err){
         if(err){
             console.log(err);
         }
 
-        return results;
+        connection.query(sql, function(err, results, _){
+            if(err){
+                console.log(err);
+            }
+
+            return results;
+        });
     });
-});
-}
+    }
+
+
 
