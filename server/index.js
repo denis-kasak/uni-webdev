3ac93476455cbb50d4dd0ff36330c4ca8cbaca96
Denis <d-kasakow@mail.de>
2023-03-08 20:59:21 +0100

    initUser() korrigiert

--- server/index.js
+++ server/index.js
@@ -7,25 +7,29 @@ const sqlmodule = require("./sql-module");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(initUser());
 
 app.get('/', (req, res) => {
+	res = initUser(req, res);
 	res.sendFile('/html/index.html', { root: 'static' });
 });
 
 app.get('/index.html', (req, res) => {
+	initUser(req);
 	res.sendFile('/html/index.html', { root: 'static' });
 });
 
 app.get('/impressum', (req, res) => {
+	initUser(req);
 	res.sendFile('/html/impressum.html', { root: 'static' });
 });
 
 app.use(express.static('static'));
 
-app.listen(8081);
+app.listen(8081, ()=>{
+	console.log("Server started on port 8081");
+});
 
-function initUser(){
+function initUser(req, res) {
 	if (req.cookies.user === undefined) {
 		const d = new Date();
 		d.setTime(d.getTime() + (20 * 365 * 24 * 60 * 60 * 1000));
@@ -33,4 +37,5 @@ function initUser(){
 		res.cookie("user", userid, { expires: d, httpOnly: true });
 		sqlmodule.addUser(userid);
 	}
+	return res;
 }
