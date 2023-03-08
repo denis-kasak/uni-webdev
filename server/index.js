083f93a6b0fa9bf24ffe3643bce20779332c9779
Denis <d-kasakow@mail.de>
2023-03-08 20:38:14 +0100

    /index.html routing + initUser() erstellt und als Middleware hinzugefügt

--- server/index.js
+++ server/index.js
@@ -7,10 +7,25 @@ const sqlmodule = require("./sql-module");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(initUser());
 
-
-app.use(express.static("Webentwicklung"));
 app.get('/', (req, res) => {
+	res.sendFile('/html/index.html', { root: 'static' });
+});
+
+app.get('/index.html', (req, res) => {
+	res.sendFile('/html/index.html', { root: 'static' });
+});
+
+app.get('/impressum', (req, res) => {
+	res.sendFile('/html/impressum.html', { root: 'static' });
+});
+
+app.use(express.static('static'));
+
+app.listen(8081);
+
+function initUser(){
 	if (req.cookies.user === undefined) {
 		const d = new Date();
 		d.setTime(d.getTime() + (20 * 365 * 24 * 60 * 60 * 1000));
@@ -18,17 +33,4 @@ app.get('/', (req, res) => {
 		res.cookie("user", userid, { expires: d, httpOnly: true });
 		sqlmodule.addUser(userid);
 	}
-	res.sendFile('/html/index.html', { root: 'static' });
-});
-
-app.get('/impressum', (req, res) => {
-	if (req.cookies.user === undefined) {
-		res.redirect('/');
-	} else {
-		res.sendFile('/html/impressum.html', { root: 'static' });
-	}
-});
-
-app.use(express.static('static'));
-
-app.listen(8081);
+}
